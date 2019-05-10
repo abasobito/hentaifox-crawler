8bcd02a64193f599f2fd087885c8dfcda5caae45
abasobito <41116079+abasobito@users.noreply.github.com>
2019-05-10 17:27:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,43 @@
 <?php
+
 require 'vendor/autoload.php';
 
 use Symfony\Component\DomCrawler\Crawler;
 
-//url
-$url = 'https://hentaifox.com/gallery/58116/';
-
 $client = new \GuzzleHttp\Client();
 
-// go get the date from url
 
-$res = $client->request('GET', $url);
+$furl = 'https://hentaifox.com';
+
+$res = $client->request('GET', $furl);
 
 $html = ''.$res->getBody();
 
 $crawler = new Crawler($html);
 
-$nameValues = $crawler->filter('.info > h1')->each(function (Crawler $node, $i) {
-$textma =  $node->text();
-$manganame = file_get_contents($textma);
-});
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, $furl);
+
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($curl);
+
+preg_match_all('!<a href="(\/gallery\/(.*?)\/)">!', $result, $manga_url);
+
+preg_match_all('!src="((.*?)\/thumb.jpg)" \/>!', $result, $image_src);
+
+foreach ($image_src[1] as $key => $image) {
+  print_r($image);
 
 
 
+?>
+
+<form method="post">
+    <input type="image" name="rateButton[1]" src="<?php echo $image ?>" width="400" height="300" value="1">T
+</form>
+
+<?php } ?>
